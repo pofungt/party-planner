f1ac2c274512d057bc28063ac74f66539cc2cee6
Duncan Tang <pofungt@hotmail.com>
2022-09-27 13:35:12 -0400

    Created tables users & events

--- party.sql
+++ party.sql
@@ -1,1 +1,27 @@
 CREATE DATABASE party;
+
+CREATE TABLE users (
+    id SERIAL primary key,
+    first_name varchar not NULL,
+    last_name varchar not NULL,
+    email varchar not NULL,
+    phone int,
+    password varchar,
+    created_at timestamp not NULL,
+    updated_at timestamp not NULL
+);
+
+CREATE TABLE events (
+    id SERIAL primary key,
+    name varchar not NULL,
+    venue varchar,
+    budget int,
+    date date,
+    start_time time,
+    end_time time,
+    creator_id int not NULL,
+    created_at timestamp not NULL,
+    updated_at timestamp not NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+);
+
