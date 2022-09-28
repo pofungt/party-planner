25fb297167031365d48e6e2006332da04784f84a
Candace <candacecsy@gmail.com>
2022-09-28 13:15:01 -0400

    login js update

--- public/landing-page/land-page.js
+++ public/landing-page/land-page.js
@@ -1,3 +1,12 @@
-document.querySelector(".login-button").addEventListener('click', function(event){
-    
+document
+    .querySelector("#login-form-submit")
+    .addEventListener('click', async function (event) {
+        const userEmail = document.querySelector("#user-email").value
+        const userPassword = document.querySelector("#user-password").value
+    if (userEmail !== "" && userPassword !== ""){
+        const res = await fetch ('/login', {
+            method: 'POST',
+        })
+        const result = await res.
+    }
 });
