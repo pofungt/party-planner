ea2a9dfe3d5cf0b789c92540452bc6923566f482
Duncan Tang <pofungt@hotmail.com>
2022-09-28 09:30:06 -0400

    Added register function to registerRoutes

--- routes/registerRoutes.ts
+++ routes/registerRoutes.ts
@@ -1,36 +1,34 @@
 import express, { Request, Response } from 'express';
 import { logger } from '../util/logger';
 import { client } from '../app';
-import { checkPassword } from '../hash';
+import { hashPassword } from '../hash';
 
 export const registerRoutes = express.Router();
 
-registerRoutes.post('/', register);
+registerRoutes.post('/', registerUser);
 
-async function register(req: Request, res: Response) {
+async function registerUser(req: Request, res: Response) {
 	try {
 		logger.debug('Before reading DB');
+    
 		const loginUser = (await client.query(
-			`SELECT * FROM users WHERE email = $1`,
-			[req.body.email]
+			`SELECT * FROM users WHERE email = $1 AND phone = $2`,
+			[req.body.email, req.body.phone]
 		)).rows[0];
 
-		if (loginUser) {
-			const match = await checkPassword(req.body.password, loginUser.password);
-			if (match) {
-				req.session.user = loginUser.id;
-				res.json({
-					status: true,
-					user: loginUser.email
-				});
-			} else {
-				res.status(401).json({status: false});
-			}
+		if (!loginUser) {
+            const password = await hashPassword(req.body.password);
+            await client.query(
+                `INSERT INTO users (first_name, last_name, email, phone, password, created_at, updated_at)
+                    VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);`,
+                [req.body.first_name, req.body.last_name, req.body.email, req.body.phone, password]
+            );
+            res.json({status: true});
 		} else {
 			res.status(401).json({status: false});
 		}
 	} catch (e) {
 		logger.error(e);
-		res.status(500).json({ msg: '[LOG001]: Failed to check Login' });
+		res.status(500).json({ msg: '[REG001]: Failed to Register' });
 	}
 }
