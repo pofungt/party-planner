ffe69433218fa9097d0915fab395a99287dd22ad
Candace <candacecsy@gmail.com>
2022-09-28 14:47:21 -0400

    correct login ()

--- public/landing-page/land-page.js
+++ public/landing-page/land-page.js
@@ -1,29 +1,27 @@
 document
     .querySelector("#login-form-submit")
-    .addEventListener('click', async function (event) {
-        const userEmail = document.querySelector("#user-email").value
-        const userPassword = document.querySelector("#user-password").value
-    if (userEmail !== "" && userPassword !== ""){
-        const formObj = {
-            'email' : userEmail,
-            'password' : userPassword
+    .addEventListener("click", async function (event) {
+        console.log("haha");
+        const userEmail = document.querySelector("#user-email").value;
+        const userPassword = document.querySelector("#user-password").value;
+        if (userEmail !== "" && userPassword !== "") {
+            const formObj = {
+                email: userEmail,
+                password: userPassword,
+            };
+            const res = await fetch("/login", {
+                method: "POST",
+                headers: {
+                    "content-Type": "application/json",
+                },
+                body: JSON.stringify(formObj),
+            });
+            const loginResult = await res.json();
+            if (loginResult.status === true) {
+                window.location = "/";
+            }
         }
-        const res = await fetch ('/login', {
-            method: 'POST',
-            headers: {
-                'content-Type': 'application/json'
-            },
-            body: JSON.stringify(formObj)
-        })
-        const loginResult = await res.json();
-        if(loginResult.status === true){
-            window.location = "/"
-        }
-    }
-
-
-
-
+    });
 
 import { registerButtons } from "./registerButtons.js";
 
