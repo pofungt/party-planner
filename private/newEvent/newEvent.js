3b17119c84860c48d606c5506cf63ba3d80da77b
Candace <candacecsy@gmail.com>
2022-09-29 12:29:15 -0400

    new event UI update

--- private/newEvent/newEvent.js
+++ private/newEvent/newEvent.js
@@ -2,5 +2,9 @@ document. querySelector("#event-submit").addEventListener("click",async function
     e.preventDefault();
     const eventName = form.
     const eventVenue = form.
+    const eventRemark = form.
 
+    let formObj = {
+        ''
+    }
 })
