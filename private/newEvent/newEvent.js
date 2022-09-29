ba6972e0aff796ef27b491fbaaf0ae8c21ee1474
Candace <candacecsy@gmail.com>
2022-09-29 16:47:25 -0400

    update date and time HTML & CSS

--- private/newEvent/newEvent.js
+++ private/newEvent/newEvent.js
@@ -1,24 +1,28 @@
 document
-    .querySelector("#event-submit")
-    .addEventListener("click", async function (e) {
+    .querySelector("#from-container")
+    .addEventListener("submit", async function (e) {
         e.preventDefault();
+        const form = e.target;
         const eventName = form.event_name.value;
         const eventVenue = form.event_venue.value;
         const indoor = form.indoor_check.checked;
         const outdoor = form.outdoor_check.checked;
-        const startDate = form.event_time_start.value;
-        const endDate = form.event_time_end.value;
+        const eventDate = form.event_date.value;
+        const startTime = form.event_time_start.value;
+        const endTime = form.event_time_end.value;
         const eventRemark = form.event_remark.value;
         const parkingLot = form.parking_check.checked;
         const lotNumber = form.lot_input.value;
 
-        let fromObj = {
+
+        let formObj = {
             eventName,
             eventVenue,
             indoor,
             outdoor,
-            startDate,
-            endDate,
+            eventDate
+            startTime,
+            endTime,
             eventRemark,
             parkingLot,
             lotNumber,
@@ -30,14 +34,15 @@ document
             dataPass = false;
             alert("Please fill in the event name!")
         }
-        
+
         if (dataPass) {
-            const res = await fetch("/", {
+            const res = await fetch("/events", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(formObj),
+            
             });
         }
     });
