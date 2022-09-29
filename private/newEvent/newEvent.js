fcb69405fe3ca1c2e72358a0084c2b6127a27b35
Candace <candacecsy@gmail.com>
2022-09-29 14:29:59 -0400

    new event page update JS for the from

--- private/newEvent/newEvent.js
+++ private/newEvent/newEvent.js
@@ -1,10 +1,13 @@
 document. querySelector("#event-submit").addEventListener("click",async function (e){
     e.preventDefault();
-    const eventName = form.
-    const eventVenue = form.
-    const eventRemark = form.
+    const eventName = form.event_name.value
+    const eventVenue = form.event_venue.value
+    const indoor = form.indoor_check.checked
+    const outdoor = form.outdoor_check.checked
+    const startDate = form.event_time_start.value
+    const endDate = form.event_time_end.value
+    const eventRemark = form.event_remark.value
+    const parkingLot = form.parking_check.checked
+    const lotNumber = form.lot_input.value
 
-    let formObj = {
-        ''
-    }
 })
