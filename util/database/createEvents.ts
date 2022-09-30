265d06eb65ebd90de877eb945317d3ea740c8ddb
Duncan Tang <pofungt@hotmail.com>
2022-09-30 13:38:10 -0400

    Created argv option for createEvents

--- util/database/createEvents.ts
+++ util/database/createEvents.ts
@@ -13,6 +13,14 @@ const client = new pg.Client({
     password: process.env.DB_PASSWORD,
   });
 
+let loopTimes: number = 1;
+const loopTimesString: string | undefined = process.argv[2];
+if (loopTimesString) {
+  if (/^\d+$/.test(loopTimesString)) {
+    loopTimes = parseInt(loopTimesString);
+  }
+}
+
 function randomDate(start: Date, days: number): Date {
     const startTime = start.getTime();
     const minusTime = startTime - days * 86_400_000;
@@ -32,38 +40,39 @@ async function main() {
 
     // Obtain users info for event creation for each user
     let users: Omit<Users, "id" | "created_at" | "updated_at">[] = await jsonfile.readFile(path.join(__dirname,"/data/users.json"));
-
-    for (const user of users) {
-        // Party name
-        const partyReason: string = parts["partyReason"][Math.floor(Math.random() * parts["partyReason"].length)];
-        const name: string = `${user.firstName}'s ${partyReason} Party`;
-        // Party venue
-        const venue: string = `${Math.floor(Math.random()*999)+1} ${parts["streetName"][Math.floor(Math.random() * parts["streetName"].length)]}`;
-        // Budget
-        const budget: number = (Math.floor(Math.random() * 10) + 1) * 1000;
-        // Date
-        const date: string = format(randomDate(new Date(), 100), 'yyyy/MM/dd');
-        const userDetail = (await client.query(`SELECT * FROM users WHERE email = $1;`,[user.email])).rows[0];
-        // Time
-        const start_time = `${randomIntFromInterval(12,17)}:${Math.random()>0.5?"00":"30"}`;
-        const end_time = `${randomIntFromInterval(18,23)}:${Math.random()>0.5?"00":"30"}`;
-        // Creator id
-        const creator_id = userDetail.id;
-
-        await client.query(
-            `INSERT INTO events 
-            (name,venue,budget,date,start_time,end_time,creator_id,created_at,updated_at) 
-            VALUES ($1,$2,$3,$4,$5,$6,$7,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);`,
-            [
-                name,
-                venue,
-                budget,
-                date,
-                start_time,
-                end_time,
-                creator_id
-            ]
-        );
+    for (let i = 0; i < loopTimes; i++) {
+        for (const user of users) {
+            // Party name
+            const partyReason: string = parts["partyReason"][Math.floor(Math.random() * parts["partyReason"].length)];
+            const name: string = `${user.firstName}'s ${partyReason} Party`;
+            // Party venue
+            const venue: string = `${Math.floor(Math.random()*999)+1} ${parts["streetName"][Math.floor(Math.random() * parts["streetName"].length)]}`;
+            // Budget
+            const budget: number = (Math.floor(Math.random() * 10) + 1) * 1000;
+            // Date
+            const date: string = format(randomDate(new Date(), 100), 'yyyy/MM/dd');
+            const userDetail = (await client.query(`SELECT * FROM users WHERE email = $1;`,[user.email])).rows[0];
+            // Time
+            const start_time = `${randomIntFromInterval(12,17)}:${Math.random()>0.5?"00":"30"}`;
+            const end_time = `${randomIntFromInterval(18,23)}:${Math.random()>0.5?"00":"30"}`;
+            // Creator id
+            const creator_id = userDetail.id;
+    
+            await client.query(
+                `INSERT INTO events 
+                (name,venue,budget,date,start_time,end_time,creator_id,created_at,updated_at) 
+                VALUES ($1,$2,$3,$4,$5,$6,$7,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);`,
+                [
+                    name,
+                    venue,
+                    budget,
+                    date,
+                    start_time,
+                    end_time,
+                    creator_id
+                ]
+            );
+        }    
     }
 
     client.end();
