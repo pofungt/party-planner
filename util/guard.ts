7f7f359284482a84eaff2d6c714971bbee6ef9c2
Yip Chak Ming, Frankie <cmyip6@gmail.com>
2022-10-03 17:11:36 -0400

    regular save

--- util/guard.ts
+++ util/guard.ts
@@ -13,3 +13,18 @@ export const isLoggedIn = (
     res.status(404).sendFile(path.resolve("./public/404.html"));
   }
 };
+
+export const isLoggedInAPI = (
+	req: express.Request,
+	res: express.Response,
+	next: express.NextFunction
+) => {
+	if (req.session?.user) {
+		//called Next here
+		console.log('user name', req.session.user);
+		next();
+	} else {
+		// redirect to 404 page
+		res.status(400).json({ error: "You don't have the permission" });
+	}
+};
