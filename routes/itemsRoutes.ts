4d6c152b5416c537f42b5ee22a94e99950235498
Duncan Tang <pofungt@hotmail.com>
2022-10-04 10:44:39 -0400

    Added empty func for itemsRoute to remove warnings

--- routes/itemsRoutes.ts
+++ routes/itemsRoutes.ts
@@ -6,8 +6,42 @@ export const itemsRoutes = express.Router();
 itemsRoutes.get("/participated", getParticipateEventList);
 itemsRoutes.get("/", getUserID);
 itemsRoutes.get("/events",getEventList);
+itemsRoutes.get("/items", getItem);
 
 async function getItem (req: Request, res: Response) {
+    try {
+        logger.debug("Before reading DB");
+
+
+    } catch(e) {
+        logger.error(e);
+        res.status(500).json({ msg: "[ITM]: Failed to post Item" });
+    }
+}
+
+async function getParticipateEventList (req: Request, res: Response) {
+    try {
+        logger.debug("Before reading DB");
+
+
+    } catch(e) {
+        logger.error(e);
+        res.status(500).json({ msg: "[ITM]: Failed to post Item" });
+    }
+}
+
+async function getUserID (req: Request, res: Response) {
+    try {
+        logger.debug("Before reading DB");
+
+
+    } catch(e) {
+        logger.error(e);
+        res.status(500).json({ msg: "[ITM]: Failed to post Item" });
+    }
+}
+
+async function getEventList (req: Request, res: Response) {
     try {
         logger.debug("Before reading DB");
 
