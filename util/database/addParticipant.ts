dd2d660debd9913d1d161735bc4de4c619c5e057
Duncan Tang <pofungt@hotmail.com>
2022-10-06 21:28:31 -0400

    Done addParticipants script

---
+++ util/database/addParticipant.ts
@@ -0,0 +1,82 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new pg.Client({
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  });
+
+let eventId: number = 0;
+const eventIdString: string | undefined = process.argv[2];
+if (eventIdString) {
+    if (/^\d+$/.test(eventIdString)) {
+        eventId = parseInt(eventIdString);
+    }
+}
+
+let participantAmount: number = 1;
+const participantAmountString: string | undefined = process.argv[3];
+if (participantAmountString) {
+  if (/^\d+$/.test(participantAmountString)) {
+    participantAmount = parseInt(participantAmountString);
+  }
+}
+
+async function main() {
+    await client.connect();
+    
+    // Get creator ID of the event (need to exclude)
+    const [creatorUserObj] = (await client.query(`
+        SELECT creator_id FROM events WHERE id = $1;
+    `,
+    [eventId]
+    )).rows;
+    const creatorUser: number = creatorUserObj.creator_id;
+
+    // Get participant ID of the event (need to exclude)
+    const participantsObj: {[key:string]: number}[] = (await client.query(`
+        SELECT user_id FROM participants
+        WHERE event_id = $1;
+    `,
+    [eventId]
+    )).rows;
+    const participants = participantsObj.map((each) => {
+        return each.user_id;
+    })
+
+    // Obtain users info for event creation for each user (excluding creator)
+    const userIdListRawObj: {[key:string]: number}[] = (await client.query(`
+        SELECT id FROM users 
+        WHERE id != $1;
+    `,
+    [creatorUser || 0]
+    )).rows;
+
+    const userIdListRaw: number[] = userIdListRawObj.map((each) => {
+        return each.id;
+    })
+    const participantsSet = new Set(participants);
+    const userIdList = userIdListRaw.filter((userId) => {
+        return !participantsSet.has(userId);
+    });
+
+    const loopTimes: number = Math.min(userIdList.length, participantAmount);
+
+    for (let i = 0; i < loopTimes; i++) {
+        const usersIndex: number = Math.floor(Math.random() * (userIdList.length));
+        const [userId] = userIdList.splice(usersIndex,1);
+        await client.query(
+          `INSERT INTO participants 
+                  (event_id,user_id,created_at,updated_at) 
+                  VALUES ($1,$2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);`,
+          [eventId, userId]
+        );
+      }
+    client.end();
+}
+  
+  main();
+  
