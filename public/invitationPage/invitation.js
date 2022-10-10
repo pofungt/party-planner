1c6fe840328a0e49742ee861f6b782854ad30c7a
Duncan Tang <pofungt@hotmail.com>
2022-10-10 01:53:40 -0400

    Finished invitation page UI

--- public/invitationPage/invitation.js
+++ public/invitationPage/invitation.js
@@ -18,6 +18,56 @@ async function checkInvitationValidity() {
 		await loadName();
 		
 		// Load invitation page content
+		document.querySelector('.event-name-container').innerHTML = `
+			<div>
+				🎉 ${result.eventDetail.name}
+			</div>
+		`;
+
+		let dateString = "";
+		if (result.eventDetail.start_datetime === "") {
+			dateString += "To Be Confirmed";
+		} else {
+			dateString += `
+				<div class="subtitle">
+					Start
+				</div>
+				<div>
+					${new Date(result.eventDetail.start_datetime)
+						.toLocaleString('en-US', { hour12: false })
+						.replace(', ', ' ')
+						.slice(0,-3)}
+				</div>
+				<div class="subtitle">
+					End
+				</div>
+				<div>
+					${new Date(result.eventDetail.end_datetime)
+						.toLocaleString('en-US', { hour12: false })
+						.replace(', ', ' ')
+						.slice(0,-3)}
+				</div>
+			`;
+		}
+		document.querySelector('.datetime-container').innerHTML = `
+			<div class="title">
+				Date & Time
+			</div>
+			<div>
+				${dateString}
+			</div>
+		`;
+
+		document.querySelector('.venue-container').innerHTML = `
+			<div class="title">
+				Venue
+			</div>
+			<div>
+				${result.eventDetail.venue === "" 
+					? "To Be Confirmed" 
+					: result.eventDetail.venue}
+			</div>
+		`;
 
 		document.body.style.display = 'block';
 	} else {
