59c6b2d0c79fb0caa377eba883efe81216759adb
Yip Chak Ming, Frankie <cmyip6@gmail.com>
2022-10-28 14:50:24 -0400

    added automation for server deployment

--- app.ts
+++ app.ts
@@ -43,7 +43,7 @@ declare module 'express-session' {
 
 const grantExpress = grant.express({
 	defaults: {
-		origin: 'https://frankieyip.world',
+		origin: 'http://localhost:8080',
 		transport: 'session',
 		state: true
 	},
